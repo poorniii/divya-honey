22d92ccb0a899d62fdcc2f957bded60adf6e680f
poorniii <35651828+poorniii@users.noreply.github.com>
2018-02-02 16:46:45 +0530

    PALINDROME.C
    check whether a number is palindrome or not.print if its a palindrome no if its not INPUTSIZE<=1000

---
+++ PALINDROME.C
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int n,reversed integer=0,remainder,original integer;
+printf("enter an integer:");
+scanf("%d",&n);
+originalInteger=n;
+while(n!=0)
+{
+remainder=n%10;
+n/10;
+}
+if(originalInteger==reverseInteger)
+printf(("%d is a palindrome",originalInteger);
+else
+printf("%d is not a palindrome",originalInteger);
+return 0;
+}

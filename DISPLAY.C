40084267f0d6e60e5c4c92876e73c4c6aa9eebc0
poorniii <35651828+poorniii@users.noreply.github.com>
2018-02-21 16:12:34 +0530

    DISPLAY.C
    given two numbers display odd numbers between two intervals

---
+++ DISPLAY.C
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int num,m=20,n=40;
+clrscr();
+printf("print odd numbers in a given range m to n:/n);
+for(num=m;num<=n;num++)
+{
+if(num%2==1)
+printf("%d",num);
+return 0;
+}

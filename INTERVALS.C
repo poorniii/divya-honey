339bcf0457d155d4e2b706caeac22897c29d8194
poorniii <35651828+poorniii@users.noreply.github.com>
2018-02-21 16:29:20 +0530

    INTERVALS.C
    display even numbers between two intervals

---
+++ INTERVALS.C
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+(
+int low,high,i,flag;
+printf("enter two numbers:");
+scanf("%d%d",&low,&high);
+printf("even number between %d&%d are:",low,high);
+while(low<high)
+{flag(i=2;i<low/2;++i)
+{
+if(low %i==0)
+{
+flag=1;
+break;
+}
+return 0;
+}

f72b25a7c2388c0f11ab61758c17fd6d7da0632e
poorniii <35651828+poorniii@users.noreply.github.com>
2018-02-23 16:43:14 +0530

    PROGRESSIVE.C
    find the sum of an arithmetic progression till n terms given n,a,d

---
+++ PROGRESSIVE.C
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<cconio.h>
+int main()
+{
+int first,diff,terms,value,sum=0,i;
+printf("enter the number of terms in AP series\n");
+scanf("enter first term and common difference of AP series\n");
+scanf("%d",&first,&diff);
+value=first;
+printf("\n sum of the AP series is %d\n",terms,sum);
+return 0;
+}

96a8798628df30ac788407cd802b87d98bb30be2
poorniii <35651828+poorniii@users.noreply.github.com>
2018-02-23 16:52:01 +0530

    ARRAY.C
    given a number N and an array of an integers print the maximum element

---
+++ ARRAY.C
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int array[],num;
+printf("enter the array name");
+scanf("%d",&num);
+printf("the array name is:");
+scanf("%d",%name);
+return 0;
+}

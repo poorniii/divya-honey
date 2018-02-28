93b9dd0dfae52831e2db36177d3a61af1f8712cd
poorniii <35651828+poorniii@users.noreply.github.com>
2018-02-28 16:07:11 +0530

    TIMES.C
    given two times hr:min format subtract them and print in the same

---
+++ TIMES.C
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main()
+{
+int hour1=6;
+int minute1-5;
+if(hour1>=0&&hour1<5)
+{
+  h=hour1-5;
+  if(h<0)
+  {
+   h=h-24; 
+  }
+}else
+{
+   h=hour1-5
+}
+printf("enter the minute:");
+if(minute1>0&& minute1<30)
+{
+  m=minute1-30;
+  if(m<0)
+  {
+    m=m+60;
+  }
+}else
+{
+  m=minute1-30;
+return 0;

e576bb9002cf268229fa28604e294c4b9dc1947a
poorniii <35651828+poorniii@users.noreply.github.com>
2018-03-09 15:49:49 +0530

    NUMERIC.C
    count the number of character in a given string

---
+++ NUMERIC.C
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+int main()
+{
+int a;
+printf("enter the helloworld 123");
+scanf("%d",&a);
+printf("enter the numeric is:3");
+scanf("%d",&3);
+return 0;
+}

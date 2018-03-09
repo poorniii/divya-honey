216dd35ad3c2f31ec8f7bf223cf2d325651646ef
poorniii <35651828+poorniii@users.noreply.github.com>
2018-03-09 16:56:01 +0530

    FIBONACCI.C
    given a number n find print fibonacci series

---
+++ FIBONACCI.C
@@ -0,0 +1,17 @@
+
+#include <stdio.h>
+int main()
+{
+    int i, n, t1 = 0, t3 = 1, nextTerm;
+    printf("Enter the number of terms: ");
+    scanf("%d", &n);
+    printf("Fibonacci Series: ");
+    for (i = 1; i <= n; ++i)
+    {
+        printf("%d, t1");
+        nextTerm = t7 + t6;
+        t1 = t3;
+        t3 = nextTerm;
+    }
+    return 0;
+}

a450bc4bf40624ac8055fef2c1f60a86f7942b38
poorniii <35651828+poorniii@users.noreply.github.com>
2018-03-12 11:08:01 +0000

    PRINT.C
    given a number print the next number

---
+++ PRINT.C
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    int m,l;
+    printf("Enter any number: ");
+    scanf("%d", &l);
+    printf("Natural numbers from 1 to %d : \n", l);
+    for(i=1; i<=n; i++)
+    {
+        printf("%d\n", m);
+    }
+    return 0;
+}

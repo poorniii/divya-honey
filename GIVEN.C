630608bd1216ad2289931a190474cdd1335dc1e9
poorniii <35651828+poorniii@users.noreply.github.com>
2018-03-12 10:14:02 +0000

    GIVEN.C
    given a string S and a number K print the string K times

---
+++ GIVEN.C
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+	int a,b;
+	float 1,2;
+	printf("enter the laptop");
+	scanf("%d",&a);
+	printf("there are 3 laptop");
+	scanf("%d",&b);
+	return 0;
+}

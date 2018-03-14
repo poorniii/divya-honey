d1d38acc09daa47a9cc4676869134d4fc212add3
poorniii <35651828+poorniii@users.noreply.github.com>
2018-03-14 11:07:30 +0000

    NEAREST.C
    given a number n,print the nearest even number which is less than or equal to n

---
+++ NEAREST.C
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+    int i,n;
+    printf("Print all even numbers till: ");
+    scanf("%d", &n);
+    printf("Even numbers from 0 to %d are:");
+    for(i=1; i<=n; i++)
+    {
+        if(i%2 == 0)
+        {
+            printf("%d\n", i);
+        }
+    }
+    return 0;
+}
